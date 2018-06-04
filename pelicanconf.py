e2d25cee30551f2dff086fdf6999964bb08f2336
Rodrigo Agundez <rragundez@gmail.com>
2018-06-04 06:11:51 -0700

    Apply spanish

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
-import os
 
 AUTHOR = 'Rodrigo Agundez'
 SITENAME = 'Rodrigo Agundez'
@@ -31,11 +30,11 @@ ARTICLE_PATHS = ['blog/en']
 
 # i18n
 I18N_SUBSITES = {
-  'de': {
-    'PAGE_PATHS': ['pages/de'],
-    'ARTICLE_PATHS': ['blog/de'],
-    'LOCALE': 'de_DE'
-  }
+    'es': {
+        'PAGE_PATHS': ['pages/es'],
+        'ARTICLE_PATHS': ['blog/es'],
+        'LOCALE': 'es_ES'
+    }
 }
 
 # logo path, needs to be stored in PATH Setting
@@ -43,63 +42,61 @@ LOGO = '/images/logo.svg'
 
 # special content
 HERO = [
-  {
-    'image': '/images/hero/background-1.jpg',
-    # for multilanguage support, create a simple dict
-    'title': {
-      'en':'Some special content',
-      'de': 'Spezieller Inhalt'
-    },
-    'text': {
-      'en': 'Any special content you want to tease here',
-      'de': 'Jeglicher spezieller Inhalt den Sie hier bewerben möchten'
-    },
-    'links': [
-      {
-        'icon': 'icon-code',
-        'url': 'https://github.com/claudio-walser/pelican-fh5co-marble',
-        'text': 'Github'
-      }
-    ]
-  }, {
-    'image': '/images/hero/background-2.jpg',
-    # keep it a string if you dont need multiple languages
-    'title': 'Uh, special too',
-    # keep it a string if you dont need multiple languages
-    'text': 'Keep hero.text and hero.title a string if you dont need multilanguage.',
-    'links': []
-  }, {
-    'image': '/images/hero/background-3.jpg',
-    'title': 'No Blogroll yet',
-    'text': 'Because of space issues in the man-nav, i didnt implemented Blogroll links yet.',
-    'links': []
-  }, {
-    'image': '/images/hero/background-4.jpg',
-    'title': 'Ads missing as well',
-    'text': 'And since i hate any ads, this is not implemented as well',
-    'links': []
-  }
+    {
+        'image': '/images/hero/background-1.jpg',
+        # for multilanguage support, create a simple dict
+        'title': {
+            'en': 'Some special content',
+            'es': 'Un contenido especial'
+        },
+        'text': {
+            'en': 'Any special content you want to tease here',
+            'es': 'Cualquier contenido especial que quieras mostrar aqui'
+        },
+        'links': [{
+            'icon': 'icon-code',
+            'url': 'https://github.com/claudio-walser/pelican-fh5co-marble',
+            'text': 'Github'
+        }]
+    }, {
+        'image': '/images/hero/background-2.jpg',
+        # keep it a string if you dont need multiple languages
+        'title': 'Uh, special too',
+        # keep it a string if you dont need multiple languages
+        'text': 'Keep hero.text and hero.title a string if you dont need multilanguage.',
+        'links': []
+    }, {
+        'image': '/images/hero/background-3.jpg',
+        'title': 'No Blogroll yet',
+        'text': 'Because of space issues in the man-nav, i didnt implemented Blogroll links yet.',
+        'links': []
+    }, {
+        'image': '/images/hero/background-4.jpg',
+        'title': 'Ads missing as well',
+        'text': 'And since i hate any ads, this is not implemented as well',
+        'links': []
+    }
 ]
 
 # Social widget
 SOCIAL = (
-  ('Github', 'https://www.github.com/rragundez'),
-  ('Twitter', 'https://www.twitter.com/rragundez'),
-  ('LinkedIn', 'https://www.linkedin.com/in/rodrigo-agundez-2b727258')
+    ('Github', 'https://www.github.com/rragundez'),
+    ('Twitter', 'https://www.twitter.com/rragundez'),
+    ('LinkedIn', 'https://www.linkedin.com/in/rodrigo-agundez-2b727258')
 )
 
 ABOUT = {
-  'image': '/images/about/about.jpeg',
-  'mail': 'info@rragundez.io',
-  # keep it a string if you dont need multiple languages
-  'text': {
-    'en': 'Learn more about the creator of this theme or just drop a message.',
-    'de': 'Lernen Sie den Author kennen oder hinterlassen Sie einfach eine Nachricht'
-  },
-  'link': 'contact.html',
-  # the address is also taken for google maps
-  'address': 'Amsterdam, The Netherlands',
-  'phone': '+31-mexicano'
+    'image': '/images/about/about.jpeg',
+    'mail': 'info@rragundez.io',
+    # keep it a string if you dont need multiple languages
+    'text': {
+        'en': 'Learn more about the creator of this theme or just drop a message.',
+        'es': 'Lernen Sie den Author kennen oder hinterlassen Sie einfach eine Nachricht'
+    },
+    'link': 'contact.html',
+    # the address is also taken for google maps
+    'address': 'Amsterdam, The Netherlands',
+    'phone': '+31-mexicano'
 }
 
 # navigation and homepage options
@@ -111,23 +108,23 @@ USE_FOLDER_AS_CATEGORY = True
 PAGE_ORDER_BY = 'order'
 
 MENUITEMS = [
-  ('Archive', 'archives.html'),
-  ('Contact', 'contact.html')
+    ('Archive', 'archives.html'),
+    ('Contact', 'contact.html')
 ]
 
 DIRECT_TEMPLATES = [
-  'index',
-  'tags',
-  'categories',
-  'authors',
-  'archives',
-  'search', # needed for tipue_search plugin
-  'contact' # needed for the contact form
+    'index',
+    'tags',
+    'categories',
+    'authors',
+    'archives',
+    'search',  # needed for tipue_search plugin
+    'contact'  # needed for the contact form
 ]
 
 # setup disqus
 DISQUS_SHORTNAME = 'gitcd-dev'
-DISQUS_ON_PAGES = False # if true its just displayed on every static page, like this you can still enable it per page
+DISQUS_ON_PAGES = False  # if true its just displayed on every static page, like this you can still enable it per page
 
 # setup google maps
 GOOGLE_MAPS_KEY = 'AIzaSyCefOgb1ZWqYtj7raVSmN4PL2WkTrc-KyA'
